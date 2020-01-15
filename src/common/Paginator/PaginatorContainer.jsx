c1bf8418eead6d0fd65395ac308b3182c793a243
Sergey <ishellyp@gmail.com>
2020-01-15 15:10:35 +0300

    feat add paginator template

---
+++ src/common/Paginator/PaginatorContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaginatorContainer = () => {
+    const [currentPage, SetCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(10)
+    return (
+        <div></div>
+    )
+}
