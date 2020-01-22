8a7b1f0ec1103ae9f9000f41768ccd67fd5b1e03
Sergey <ishellyp@gmail.com>
2020-01-22 21:03:51 +0300

    feat: added redux chrome dev tools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -8,6 +8,7 @@ import { mixesReducer } from "../component/Mixes/mixes-reducer";
 import { firestoreReducer } from 'redux-firestore'
 import { firebaseReducer, getFirebase } from 'react-redux-firebase'
 import { producersReducer } from './../component/Side/producers-reducer';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 const persistConfig = {
     key: 'root',
@@ -26,8 +27,7 @@ let rootReducers = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducers)
 
 export let store = createStore(persistedReducer,
-    compose(
-        applyMiddleware(thunk.withExtraArgument(getFirebase))
+    composeWithDevTools(applyMiddleware(thunk.withExtraArgument(getFirebase))
     )
 )
 export let persistor = persistStore(store)
