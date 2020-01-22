d06d16fa0327b1b78d5cb2964068c1d9fc13ee5d
Sergey <ishellyp@gmail.com>
2020-01-22 19:42:08 +0300

    fix: added margin bot and left  for tablets

--- src/component/TobacoList/tobacoList.styled.js
+++ src/component/TobacoList/tobacoList.styled.js
@@ -26,11 +26,17 @@ padding: 1%;
 margin-left: 4%;
 margin-bottom: 4%;
 max-width: 190px;
+cursor: pointer;
+border: 1px solid #bdc3c7;
+border-radius: 7px;
 &:hover {
     border-color: ${({ theme }) => theme.primaryHover}; 
   }
-  cursor: pointer;
-  border: 1px solid #bdc3c7;
-  border-radius: 7px
+  
+
+  @media ${({ theme }) => theme.device.tablet} {
+  margin-left: 1em;
+  margin-bottom: 1em;
+  }
 
 `
